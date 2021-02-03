d5c2f66541104d36b1e4eca60006091a8784cab9
root <root@cloud.tigaserangkaiteknik.com>
2021-02-03 18:28:16 -0500

    update contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 const IndexPage = () => (
+    <div> 
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <form 
 	name="contact" 
 	method="post"
@@ -14,6 +14,7 @@ const IndexPage = () => (
       <input name="name" placeholder="Your Name" type="text" />
       <button>Send</button>
     </form>
+    </div>
 )
 
 export default IndexPage
