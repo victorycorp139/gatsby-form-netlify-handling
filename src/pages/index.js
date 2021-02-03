4cbfcdea2acdd56861a7810aaa1b9aef7c1ee4ab
root <root@cloud.tigaserangkaiteknik.com>
2021-02-03 18:38:24 -0500

    update netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
 const IndexPage = () => (
     <div> 
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <form 
-	name="contact" 
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <form 
+        name="contact" 
 	method="post"
 	data-netlify="true"
 	data-netlify-honeypot="bot-field"
-    >
+      >
       <input name="name" placeholder="Your Name" type="text" />
+      <input name="email" placeholder="name@name.com" type="email" />
+      <textarea name="message" />
       <button>Send</button>
     </form>
     </div>
